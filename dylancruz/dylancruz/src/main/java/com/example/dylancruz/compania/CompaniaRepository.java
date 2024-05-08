9d85bd9a374abf851d73effa1f782995d024d820
DYLAN7cruz <119619240+DYLAN7cruz@users.noreply.github.com>
2024-05-08 00:25:08 -0500

    Add files via upload

---
+++ dylancruz/dylancruz/src/main/java/com/example/dylancruz/compania/CompaniaRepository.java
@@ -0,0 +1,8 @@
+package com.example.dylancruz.compania;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompaniaRepository extends CrudRepository<Compania, Long>{
+    List<Compania> findAll();
+}
---
+++ dylancruz/dylancruz/src/test/java/com/example/dylancruz/DylancruzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dylancruz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DylancruzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
